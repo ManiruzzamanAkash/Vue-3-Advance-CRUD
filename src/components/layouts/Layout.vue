0f4747af78e677ca63f15078b158bec1915ee6f2
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-02-22 17:32:42 +0600

    About Page added

--- src/components/layouts/Layout.vue
+++ src/components/layouts/Layout.vue
@@ -1,24 +1,26 @@
 <template>
   <div id="layout">
     <Header />
-    <div class="container mt-3">
-      <router-view />
+    <div class="main-content">
+      <div class="container mt-3">
+        <router-view />
+      </div>
     </div>
     <Footer />
   </div>
 </template>
 
 <script>
-import Footer from './footer/Footer';
-import Header from './header/Header';
+import Footer from "./footer/Footer";
+import Header from "./header/Header";
 export default {
-  name: 'Layout',
+  name: "Layout",
   props: {
-    msg: String
+    msg: String,
   },
   components: {
     Header,
-    Footer
-  }
-}
+    Footer,
+  },
+};
 </script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h4>About This Project</h4>
+    <hr>
+    <div>
+      <p>Hi, It's a project with Complete Vue Single Page Application (SPA). Which  includes the following parts :</p>
+      <p>
+        <ul>
+          <li>Vue JS</li>
+          <li>Vuex Store Mangement</li>
+          <li>Complete CRUD Application</li>
+          <li>Sweet Alert</li>
+          <li>And Many More...</li>
+        </ul>
+      </p>
+    </div>
   </div>
 </template>
