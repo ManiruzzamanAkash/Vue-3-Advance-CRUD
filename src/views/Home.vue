a02c71469491afd9aabd48f3af4b94e5df3d4426
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-02-23 16:33:58 +0600

    Total Count showed in frontend

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
         <div class="col-md-2">
           <router-link to="/products" class="btn btn-primary p-4 dashboard-widget">
             All Products
-            <span class="badge badge-success">
-              20
+            <span class="badge badge-success" v-if="productsPaginatedData !== null"> 
+              {{ productsPaginatedData.pagination.total }}  
             </span>
           </router-link>
         </div>
@@ -17,14 +17,27 @@
         </div>
       </div>
     </div>
+    <div>
+      <product-list-component></product-list-component>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from "vuex";
+import ProductListComponent from '../components/products/list/ProductListComponent.vue';
 export default {
-  name: 'Home',
-  components: {
-    
-  }
-}
+  components: { ProductListComponent },
+  name: "Home",
+
+  computed: { ...mapGetters(["productsPaginatedData"]) },
+
+  methods: {
+    ...mapActions(["fetchAllProducts"]),
+  },
+
+  created() {
+    this.fetchAllProducts(1);
+  },
+};
 </script>
