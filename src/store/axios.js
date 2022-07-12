2515744976bf5df8bcd99c93967e6330900a71b7
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2022-07-12 14:00:08 +0600

    Managed backend API key from env file

--- src/store/axios.js
+++ src/store/axios.js
@@ -5,7 +5,7 @@ axios.interceptors.request.use(
     config => {
         const token = 'localStorageService.getAccessToken()';
         if (token) {
-            config.headers['Authorization'] = 'Bearer ' + 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC8xMjcuMC4wLjE6ODAwMFwvYXBpXC9hdXRoXC9sb2dpbiIsImlhdCI6MTYxMzk5MDE5MiwiZXhwIjoxNjE2NTgyMTkyLCJuYmYiOjE2MTM5OTAxOTIsImp0aSI6IkNvMGU4emtMTnE3Q2hIcWciLCJzdWIiOjEsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.ssafhxfR5bhTrnX5Wph2feMkHGCLOtJ_4TiEZwrJ1i8';
+            config.headers['Authorization'] = `Bearer ${process.env.VUE_APP_API_KEY}`;
         }
         return config;
     },
